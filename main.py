2560f5b99d30d59f5b111f9ec55b4d993d49a8e9
amrusiy <103115266+amrusiy@users.noreply.github.com>
2022-04-28 14:10:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ocr_non_english import *
 
 
 def main():
+    count = 0
     files_names = get_names_of_all_the_pdf_file()
     for i in range(len(files_names)):
         flage = 1
@@ -14,7 +15,7 @@ def main():
         try:
             Converting_PDF_to_jpeg(fm)  # create image name "page_1.png"
             improve_contrast()
-            text = Recognizing_text_from_the_images_using_OCR("heb")
+            text = Recognizing_text_from_the_images_using_OCR("heb_eng")
 
         except:
             print("Error: Converting_PDF_to_jpeg or Recognizing_text_from_the_images_using_OCR")
@@ -22,13 +23,18 @@ def main():
             pass
 
         if flage != 0:
-            flage = find_match_in_excel_extract_data_and_transfer_file_name_it(text, files_names[i])
+           flage = find_match_in_excel_extract_data_and_transfer_file_name_it(text, files_names[i])
         if flage == 0:
             print("The sort didn't Succeeded please sort manualy")
             print("")
         else:
             print("The sort Succeeded  (^o^)(^_^ ) ")
+            count = count +1
             print("")
+    print("")
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    print("Number of PDF: "+str(len(files_names))+" | Number of success: "+ str(count))
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 
 if __name__ == "__main__":
     main()
